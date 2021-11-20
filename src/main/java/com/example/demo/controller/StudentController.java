b85b7c9582cb3ee64f6a650cbbda3a909d008571
rifan <rifan@ecquaria.com>
2021-11-20 13:29:38 +0700

    'initialize'

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/demos")
+public class StudentController {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewStudent(@RequestParam String name, @RequestParam String className, 
+        @RequestParam Integer age, @RequestParam String birthday) {
+            Student student = new Student();
+            student.setName(name);
+            student.setClassName(className);
+            student.setAge(age);
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd"); 
+            Date bd = null;
+            try {
+                bd = df.parse(birthday);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            student.setBirthday(bd);
+            studentRepository.save(student);
+            return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Student> getAllStudens() {
+        return studentRepository.findAll();
+    }
+}
---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Student {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String className;
+    private Integer age;
+    private Date birthday;
+
+    
+}
